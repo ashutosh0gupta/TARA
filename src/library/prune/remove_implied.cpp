70fb1ece4ff33a90d8039f3760859d7c325ef173
Ashutosh Gupta <agupta@tifr.res.in>
2016-01-21 04:44:55 -0500

    remove_implied modfied

--- src/library/prune/remove_implied.cpp
+++ src/library/prune/remove_implied.cpp
@@ -32,9 +32,10 @@ remove_implied::remove_implied(const z3interf& z3, const cssa::program& program)
 //start of wmm support
 //--------------------------------------------------------------------------
   if( program.is_mm_declared() ) {
-    if( program.is_mm_tso() ){
+    if( program.is_mm_tso() || program.is_mm_sc() ){
     }else{
-      throw std::runtime_error("unsupported");
+      throw std::runtime_error(
+        "remove_imples does not unsupport the give meory model!");
     }
   }
 //--------------------------------------------------------------------------
@@ -47,6 +48,35 @@ string remove_implied::name()
   return string("remove_implied");
 }
 
+bool remove_implied::compare_sc_events( const hb_enc::location_ptr loc1,
+                                        const hb_enc::location_ptr loc2 ) {
+  // check if these edge is between the same thread
+  if( loc1->thread != loc2->thread ) return false;
+  // check if the other is from earlier instruction
+  if( loc1->instr_no < loc2-> instr_no ) return true;
+  // if they are from same instruction then they must be Rd-Wr
+  if( loc1->instr_no == loc2->instr_no ) {
+    if( loc1 == loc2 ) return true;
+    if( loc1->is_read && !loc2->is_read ) return true;
+  }
+  return false;
+}
+
+bool remove_implied::compare_tso_events( const hb_enc::location_ptr loc1,
+                                         const hb_enc::location_ptr loc2 ) {
+  // check if these edge is between the same thread
+  if( loc1->thread != loc2->thread ) return false;
+  // check if the other one is more specific
+  if( !loc1->is_read && loc2->is_read ) return false;
+
+  if( loc1->instr_no < loc2-> instr_no ) return true;
+  if( loc1->instr_no == loc2->instr_no ) {
+    if( loc1 == loc2 ) return true;
+    if( loc1->is_read && !loc2->is_read ) return true;
+  }
+  return false;
+}
+
 list< z3::expr > remove_implied::prune(const list< z3::expr >& hbs, const z3::model& m)
 {
   list<z3::expr> result = hbs;
@@ -54,7 +84,34 @@ list< z3::expr > remove_implied::prune(const list< z3::expr >& hbs, const z3::mo
 //start of wmm support
 //--------------------------------------------------------------------------
   if( program.is_mm_declared() ) {
-    if( program.is_mm_tso() ) {
+     if( program.is_mm_sc() ) {
+       for (auto it = result.begin() ; it != result.end(); ) {
+         bool remove = false;
+         for (auto it2 = result.begin() ; it2 != result.end(); ++it2) {
+           // ensure that we do not compare with ourselves
+           if (it != it2) {
+             // find duplicate
+             if ((Z3_ast)*it == (Z3_ast)*it2) {
+               remove = true;
+               break;
+             }
+             unique_ptr<hb_enc::hb> hb1 = hb_enc.get_hb(*it);
+             unique_ptr<hb_enc::hb> hb2 = hb_enc.get_hb(*it2);
+             assert (hb1 && hb2);
+             if( compare_sc_events( hb1->loc1, hb2->loc1 ) &&
+                 compare_sc_events( hb2->loc2, hb1->loc2 ) ) {
+                   remove = true;
+                   break;
+             }
+           }
+         }
+         if (remove) {
+           //cerr << *it << endl;
+           it = result.erase(it);
+         }else
+           ++it;
+       }
+     }else if( program.is_mm_tso() ) {
       for (auto it = result.begin() ; it != result.end(); ) {
         bool remove = false;
         for (auto it2 = result.begin() ; it2 != result.end(); ++it2) {
@@ -68,19 +125,21 @@ list< z3::expr > remove_implied::prune(const list< z3::expr >& hbs, const z3::mo
             unique_ptr<hb_enc::hb> hb1 = hb_enc.get_hb(*it);
             unique_ptr<hb_enc::hb> hb2 = hb_enc.get_hb(*it2);
             assert (hb1 && hb2);
-            // check if these edge is between the same thread
-            if( hb1->loc1->thread == hb2->loc1->thread &&
-                 hb1->loc2->thread == hb2->loc2->thread ) {
-              // check if the other one is more specific
-              if( hb1->loc1->instr_no <= hb2->loc1->instr_no &&
-                  ( hb1->loc1->is_read || !hb2->loc1->is_read ) &&
-                  hb1->loc2->instr_no >= hb2->loc2->instr_no &&
-                  ( hb2->loc2->is_read || !hb1->loc2->is_read ) )
+             if( compare_tso_events( hb1->loc1, hb2->loc1 ) &&
+                 compare_tso_events( hb2->loc2, hb1->loc2 ) )
+            // // check if these edge is between the same thread
+            // if( hb1->loc1->thread == hb2->loc1->thread &&
+            //      hb1->loc2->thread == hb2->loc2->thread ) {
+            //   // check if the other one is more specific
+            //   if( hb1->loc1->instr_no <= hb2->loc1->instr_no &&
+            //       ( hb1->loc1->is_read || !hb2->loc1->is_read ) &&
+            //       hb1->loc2->instr_no >= hb2->loc2->instr_no &&
+            //       ( hb2->loc2->is_read || !hb1->loc2->is_read ) )
                 {
                   remove = true;
                   break;
                 }
-            }
+            // }
           }
         }
         if (remove) {
--- src/library/prune/remove_implied.h
+++ src/library/prune/remove_implied.h
@@ -32,6 +32,16 @@ public:
   virtual std::list< z3::expr > prune(const std::list< z3::expr >& hbs, const z3::model& m);
   virtual std::string name();
 private:
+//--------------------------------------------------------------------------
+//start of wmm support
+//--------------------------------------------------------------------------
+  bool compare_sc_events( const hb_enc::location_ptr loc1,
+                          const hb_enc::location_ptr loc2 );
+  bool compare_tso_events( const hb_enc::location_ptr loc1,
+                           const hb_enc::location_ptr loc2 );
+//--------------------------------------------------------------------------
+//end of wmm support
+//--------------------------------------------------------------------------
 };
 }}
 
