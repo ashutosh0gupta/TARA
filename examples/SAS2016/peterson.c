3026e05f7002dd2059f81477b64318b675190938
ShikharDPandya <shikharpandya5@gmail.com>
2016-04-07 23:59:15 +0530

    added files from musketeer

---
+++ examples/SAS2016/peterson.c
@@ -0,0 +1,27 @@
+/* Copied from musketeer */
+int turn; // integer variable to hold the ID of the thread whose turn is it
+int flag1 = 0, flag2 = 0; // boolean flags
+
+void* thr1(void * arg) { // frontend produces 12 transitions from this thread. It would be better if it would produce only 8!
+  flag1 = 1;
+  turn = 1;
+  do {} while (flag2==1 && turn==1);
+  // begin: critical section
+  // end: critical section
+  flag1 = 0;
+}
+
+void* thr2(void * arg) {
+  flag2 = 1;
+  turn = 0;
+  do {} while (flag1==1 && turn==0);
+  // begin: critical section
+  // end: critical section
+  flag2 = 0;
+}
+  
+int main()
+{
+  __CPROVER_ASYNC_1: thr1(0);
+  thr2(0);
+}
