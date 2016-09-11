13f89f2bf1597c71739793005f1e1a3ea2166251
Ashutosh Gupta <agupta@tifr.res.in>
2016-09-11 11:42:10 +0530

    conflict removed

--- examples/vmcai2016/peterson.cpp
+++ examples/vmcai2016/peterson.cpp
@@ -42,10 +42,6 @@ int main() {
   pthread_create(&thr_1, NULL, p1, NULL );
   pthread_join(thr_0, NULL);
   pthread_join(thr_1, NULL);
-<<<<<<< HEAD
-  assert(!((done0 == 1) && (done1 == 1)) || cs == 2);
-=======
   assert(!((flag0 == 0) && (flag1 == 0)) || cs == 2 );
->>>>>>> 25ec2530a1cd22d031575d97985d0970a77f327b
 }
 
